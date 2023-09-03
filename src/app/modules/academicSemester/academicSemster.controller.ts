d913a31a8038e1ce1cb1d9587fe87e682785346e
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-04 04:38:37 +0600

    Improved Functionality with catchAsync

--- src/app/modules/academicSemester/academicSemster.controller.ts
+++ src/app/modules/academicSemester/academicSemster.controller.ts
@@ -1,26 +1,19 @@
 import { AcademicSemester } from '@prisma/client';
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { AcademicSemesterService } from './academicSemester.service';
 
-const insertIntoDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const result = await AcademicSemesterService.insertIntoDB(req.body);
-    sendResponse<AcademicSemester>(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Academic Semester Created!!',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.insertIntoDB(req.body);
+  sendResponse<AcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester Created!!',
+    data: result,
+  });
+});
 
 export const AcademicSemesterController = {
   insertIntoDB,
