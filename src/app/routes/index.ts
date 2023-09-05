902df17c37887569eab95d7045b65c69b91b1530
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-06 03:16:29 +0600

    Added Academic Department Module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
 
 const router = express.Router();
@@ -8,6 +9,10 @@ const moduleRoutes = [
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: academicDepartmentRoutes,
   },
 ];
 
