b20d5ee181dbd1b9884c44bff3df3a0b95139bff
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-08 04:30:55 +0600

    Assigning API Endpoint Permissions by Role and Updated Academic Semester Module With Update & Delete Service

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemster.controller';
 import { AcademicSemesterValidation } from './academicSemster.validation';
@@ -11,6 +13,19 @@ router.post(
   '/',
   validateRequest(AcademicSemesterValidation.create),
   AcademicSemesterController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  validateRequest(AcademicSemesterValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteByIdFromDB
 );
 
 export const AcademicSemesterRoutes = router;
