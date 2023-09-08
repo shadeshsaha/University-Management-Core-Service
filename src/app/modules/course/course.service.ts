053d0ad35b0ddaeaaac00d9a16844f759ccb4d08
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-09 02:52:00 +0600

    Implemented Creating New Course & Implementing Transactional Support for Course Creation

---
+++ src/app/modules/course/course.service.ts
@@ -0,0 +1,66 @@
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import prisma from '../../../shared/prisma';
+import { ICourseCreateData } from './course.interface';
+
+const insertIntoDB = async (
+  data: ICourseCreateData
+): Promise<ICourseCreateData> => {
+  const { preRequisiteCourses, ...courseData } = data;
+  // console.log(preRequisiteCourses);
+  // console.log(courseData);
+
+  // Started Transaction
+  const newCourse = await prisma.$transaction(async transactionClient => {
+    const result = await transactionClient.course.create({
+      data: courseData,
+    });
+
+    if (!result) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Unable To Create New Course');
+    }
+
+    if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+      for (let index = 0; index < preRequisiteCourses.length; index++) {
+        const createPrerequisite =
+          await transactionClient.courseToPrerequisite.create({
+            data: {
+              courseId: result.id,
+              preRequisiteId: preRequisiteCourses[index].courseId,
+            },
+          });
+        console.log('createPrerequisite', createPrerequisite);
+      }
+    }
+
+    return result;
+  });
+
+  if (newCourse) {
+    const responseData = await prisma.course.findUnique({
+      where: {
+        id: newCourse.id,
+      },
+      include: {
+        preRequisite: {
+          include: {
+            preRequisite: true,
+          },
+        },
+        preRequisiteFor: {
+          include: {
+            course: true,
+          },
+        },
+      },
+    });
+
+    return responseData;
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Unable To Create New Course');
+};
+
+export const CourseService = {
+  insertIntoDB,
+};
