6282dc5d9f830824369b4dd3352c3b04719f752e
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-08 07:22:10 +0600

    Added Role-based API Access

--- src/app/modules/building/building.routes.ts
+++ src/app/modules/building/building.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
 import { BuildingValidations } from './building.validation';
@@ -9,6 +11,7 @@ router.get('/', BuildingController.getAllFromDB);
 router.post(
   '/',
   validateRequest(BuildingValidations.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   BuildingController.insertIntoDB
 );
 
