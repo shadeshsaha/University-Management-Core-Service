ae86648ffeff5b6cafca0d71c5ac1ce43c425870
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-08 07:19:31 +0600

    Added Create Building Validation

--- src/app/modules/building/building.routes.ts
+++ src/app/modules/building/building.routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
+import { BuildingValidations } from './building.validation';
 
 const router = express.Router();
 
 router.get('/', BuildingController.getAllFromDB);
-router.post('/', BuildingController.insertIntoDB);
+router.post(
+  '/',
+  validateRequest(BuildingValidations.create),
+  BuildingController.insertIntoDB
+);
 
 export const buildingRoutes = router;
