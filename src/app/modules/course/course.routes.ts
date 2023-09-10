22767d4f43a723e371fe02c385292983a4d53e16
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-11 03:06:00 +0600

    Implemented Get All Course Data(Filter Data), Get Course By ID in Course Module

--- src/app/modules/course/course.routes.ts
+++ src/app/modules/course/course.routes.ts
@@ -3,6 +3,9 @@ import { CourseController } from './course.controller';
 
 const router = express.Router();
 
+router.get('/', CourseController.getAllFromDB);
+router.get('/:id', CourseController.getByIdFromDB);
+
 router.post('/', CourseController.insertIntoDB);
 
 export const courseRoutes = router;
