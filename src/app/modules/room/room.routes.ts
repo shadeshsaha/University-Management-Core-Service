4753b4b83e976e886abf875610c58aa4b67dd4aa
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-11 06:02:58 +0600

    Added Validation On Room & Added Role On Routes In Room Module

--- src/app/modules/room/room.routes.ts
+++ src/app/modules/room/room.routes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { RoomController } from './room.controller';
+import { RoomValidation } from './room.validations';
 
 const router = express.Router();
 
@@ -10,20 +12,21 @@ router.get('/:id', RoomController.getByIdFromDB);
 
 router.post(
   '/',
+  validateRequest(RoomValidation.create),
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   RoomController.insertIntoDB
 );
 
 router.patch(
   '/:id',
-  // validateRequest(RoomValidation.update),
-  // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(RoomValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   RoomController.updateOneInDB
 );
 
 router.delete(
   '/:id',
-  // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   RoomController.deleteByIdFromDB
 );
 
