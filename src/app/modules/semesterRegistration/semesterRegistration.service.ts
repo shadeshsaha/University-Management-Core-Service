5963b16a0cc49aeb16f89ef7fa06aab867a33f1c
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-15 03:11:52 +0600

    Added Validation in Semester Registration Service

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,9 +1,35 @@
-import { SemesterRegistration } from '@prisma/client';
+import {
+  SemesterRegistration,
+  SemesterRegistrationStatus,
+} from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
 
 const insertIntoDB = async (
   data: SemesterRegistration
 ): Promise<SemesterRegistration> => {
+  const isAnySemesterRegistrationUpcomingOrOngoing =
+    await prisma.semesterRegistration.findFirst({
+      where: {
+        OR: [
+          {
+            status: SemesterRegistrationStatus.UPCOMING,
+          },
+          {
+            status: SemesterRegistrationStatus.ONGOING,
+          },
+        ],
+      },
+    });
+
+  if (isAnySemesterRegistrationUpcomingOrOngoing) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      `There is already an ${isAnySemesterRegistrationUpcomingOrOngoing.status} registration`
+    );
+  }
+
   const result = await prisma.semesterRegistration.create({
     data,
   });
