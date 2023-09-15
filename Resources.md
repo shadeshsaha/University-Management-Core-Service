442693cb680c6057fdab4a04af330361d58d1406
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-16 04:29:52 +0600

    Added Referencing Error Messages in Prisma [PrismaClientKnownRequestError]

--- Resources.md
+++ Resources.md
@@ -20,3 +20,5 @@
 
 - **You can generate an ERD with Prisma schema using the website: https://prismaliser.app/**
 - [API Documentation](https://documenter.getpostman.com/view/26694209/2s9Xy5NrFU)
+
+- [Prisma Errors](https://www.prisma.io/docs/reference/api-reference/error-reference)
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,7 +6,7 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import { ZodError } from 'zod';
 import config from '../../config';
 import ApiError from '../../errors/ApiError';
-import handleCastError from '../../errors/handleCastError';
+import handleClientError from '../../errors/handleClientError';
 import handleValidationError from '../../errors/handleValidationError';
 import handleZodError from '../../errors/handleZodError';
 import { IGenericErrorMessage } from '../../interfaces/error';
@@ -26,23 +26,29 @@ const globalErrorHandler: ErrorRequestHandler = (
   let message = 'Something went wrong !';
   let errorMessages: IGenericErrorMessage[] = [];
 
+  // <-- Prisma Client Validation Error
   if (error instanceof Prisma.PrismaClientValidationError) {
-    // <-- Prisma Client Validation Error
     const simplifiedError = handleValidationError(error);
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
     errorMessages = simplifiedError.errorMessages;
-  } else if (error instanceof ZodError) {
+  }
+  // Zod Error
+  else if (error instanceof ZodError) {
     const simplifiedError = handleZodError(error);
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
     errorMessages = simplifiedError.errorMessages;
-  } else if (error?.name === 'CastError') {
-    const simplifiedError = handleCastError(error);
+  }
+  // <-- Prisma Client Known Request Error
+  else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    const simplifiedError = handleClientError(error);
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
     errorMessages = simplifiedError.errorMessages;
-  } else if (error instanceof ApiError) {
+  }
+  // API Error
+  else if (error instanceof ApiError) {
     statusCode = error?.statusCode;
     message = error.message;
     errorMessages = error?.message
@@ -53,7 +59,9 @@ const globalErrorHandler: ErrorRequestHandler = (
           },
         ]
       : [];
-  } else if (error instanceof Error) {
+  }
+  // Error
+  else if (error instanceof Error) {
     message = error?.message;
     errorMessages = error?.message
       ? [
