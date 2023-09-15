e27c3840e80635430425f30d1693b1dd5bd424ce
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-16 03:01:53 +0600

    Implemented Role Authorization and Zod Validation for Update Semester Registration

--- src/app/modules/semesterRegistration/semesterRegistration.validations.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.validations.ts
@@ -1,3 +1,4 @@
+import { SemesterRegistrationStatus } from '@prisma/client';
 import { z } from 'zod';
 
 const create = z.object({
@@ -20,6 +21,23 @@ const create = z.object({
   }),
 });
 
+const update = z.object({
+  body: z.object({
+    startDate: z.string().optional(),
+    endDate: z.string().optional(),
+    academicSemesterId: z.string().optional(),
+    minCredit: z.number().optional(),
+    maxCredit: z.number().optional(),
+    status: z
+      .enum(
+        [...Object.values(SemesterRegistrationStatus)] as [string, ...string[]],
+        {}
+      )
+      .optional(),
+  }),
+});
+
 export const SemesterRegistrationValidation = {
   create,
+  update,
 };
