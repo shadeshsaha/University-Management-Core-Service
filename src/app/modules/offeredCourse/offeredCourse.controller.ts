59b76b056c5b945c34430f02e5a8c2ce6a270279
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-16 17:57:46 +0600

    Implemented Create Offered Course

---
+++ src/app/modules/offeredCourse/offeredCourse.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { OfferedCourseService } from './offeredCourse.service';
+
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  // console.log(req.body);
+  const result = await OfferedCourseService.insertIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Offered Course Created Successfully',
+    data: result,
+  });
+});
+
+export const OfferedCourseController = {
+  insertIntoDB,
+};
