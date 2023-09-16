b1c5d9073afc98572da27325957f7fd0e2441b5c
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-16 18:29:28 +0600

    Added Validation Of Not Creating Duplicate Offered Course

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -7,16 +7,34 @@ const insertIntoDB = async (
   data: ICreateOfferedCourse
 ): Promise<OfferedCourse[]> => {
   const { academicDepartmentId, semesterRegistrationId, courseIds } = data;
-  const result: any[] = [];
+  const result: OfferedCourse[] = [];
+
   await asyncForEach(courseIds, async (courseId: string) => {
-    const insertOfferedCourse = await prisma.offeredCourse.create({
-      data: {
+    // jei id gulo diye request kortesi,shei id gulo data gulo db te exist kortese kina. thakle create korbo na
+    const alreadyExist = await prisma.offeredCourse.findFirst({
+      where: {
         academicDepartmentId,
         semesterRegistrationId,
         courseId,
       },
     });
-    result.push(insertOfferedCourse);
+
+    // Data exist na thakle create korbe
+    if (!alreadyExist) {
+      const insertOfferedCourse = await prisma.offeredCourse.create({
+        data: {
+          academicDepartmentId,
+          semesterRegistrationId,
+          courseId,
+        },
+        include: {
+          academicDepartment: true,
+          semesterRegistration: true,
+          course: true,
+        },
+      });
+      result.push(insertOfferedCourse);
+    }
   });
   return result;
 };
