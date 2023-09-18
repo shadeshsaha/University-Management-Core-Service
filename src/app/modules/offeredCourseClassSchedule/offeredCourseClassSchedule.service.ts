a0836b04835e30c40fb01f53f13eec9e2b925fd6
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-09-19 01:47:30 +0600

    Implemented Room Availability Check for Inserting Offered Course Class Schedule

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.service.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.service.ts
@@ -2,6 +2,7 @@ import { OfferedCourseClassSchedule } from '@prisma/client';
 import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
+import { hasTimeConflict } from '../../../shared/utils';
 
 const insertIntoDB = async (
   data: OfferedCourseClassSchedule
@@ -33,22 +34,8 @@ const insertIntoDB = async (
   // console.log('existingSlots: ', existingSlots);
   // console.log('newSlot:', newSlot);
 
-  for (const slot of existingSlots) {
-    const existingStartTime = new Date(`1970-01-01T${slot.startTime}:00`);
-    const existingEndTime = new Date(`1970-01-01T${slot.endTime}:00`);
-    const newStartTime = new Date(`1970-01-01T${newSlot.startTime}:00`);
-    const newEndTime = new Date(`1970-01-01T${newSlot.endTime}:00`);
-
-    // console.log('existingStartTime:', existingStartTime);
-    // console.log('existingEndTime:', existingEndTime);
-    // console.log('newStartTime:', newStartTime);
-    // console.log('newEndTime:', newEndTime);
-
-    // existing: 12:30 - 13:30
-    // new slot: 12:50 - 13:50 => eta fillup korle nicher error ta dekhabe
-    if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
-      throw new ApiError(httpStatus.CONFLICT, 'Room is already booked!');
-    }
+  if (hasTimeConflict(existingSlots, newSlot)) {
+    throw new ApiError(httpStatus.CONFLICT, 'Room is already booked!');
   }
 
   const result = await prisma.offeredCourseClassSchedule.create({
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,5 @@
+import { WeekDays } from '@prisma/client';
+
 export const asyncForEach = async (array: any[], callback: any) => {
   if (!Array.isArray(array)) {
     throw new Error('Expected an array');
@@ -5,4 +7,37 @@ export const asyncForEach = async (array: any[], callback: any) => {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
   }
+};
+
+export const hasTimeConflict = (
+  existingSlots: {
+    startTime: string;
+    endTime: string;
+    dayOfWeek: WeekDays;
+  }[],
+  newSlot: {
+    startTime: string;
+    endTime: string;
+    dayOfWeek: WeekDays;
+  }
+) => {
+  for (const slot of existingSlots) {
+    const existingStartTime = new Date(`1970-01-01T${slot.startTime}:00`);
+    const existingEndTime = new Date(`1970-01-01T${slot.endTime}:00`);
+    const newStartTime = new Date(`1970-01-01T${newSlot.startTime}:00`);
+    const newEndTime = new Date(`1970-01-01T${newSlot.endTime}:00`);
+
+    // console.log('existingStartTime:', existingStartTime);
+    // console.log('existingEndTime:', existingEndTime);
+    // console.log('newStartTime:', newStartTime);
+    // console.log('newEndTime:', newEndTime);
+
+    // existing: 12:30 - 13:30
+    // new slot: 12:50 - 13:50 => eta fillup korle nicher error ta dekhabe
+    if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+      // throw new ApiError(httpStatus.CONFLICT, 'Room is already booked!');
+      return true;
+    }
+  }
+  return false;
 };
