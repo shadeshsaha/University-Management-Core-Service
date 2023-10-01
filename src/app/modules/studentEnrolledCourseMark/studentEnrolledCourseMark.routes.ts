3ddbcf2b563ad857527f61d5a4ff2a91cfd23056
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-10-02 03:43:23 +0600

    Set Role On Student Enrolled Course Marks Routes (Get All)

--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.routes.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { StudentEnrolledCourseMarkController } from './studentEnrolledCourseMark.controller';
 
 const router = express.Router();
 
-router.get('/', StudentEnrolledCourseMarkController.getAllFromDB);
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.FACULTY),
+  StudentEnrolledCourseMarkController.getAllFromDB
+);
 
 router.patch(
   '/update-marks',
